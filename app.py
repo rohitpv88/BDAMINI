91fd1d8f358043069fcecacfc179479af3ff4939
rohitpv88 <166278360+rohitpv88@users.noreply.github.com>
2024-11-10 17:04:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, request, jsonify, render_template
 from werkzeug.utils import secure_filename
 import os
 
-
 app = Flask(__name__)
-
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+if __name__ == '__main__':
+    # Get the port from the environment variable, or default to 5000 if not set
+    port = int(os.environ.get("PORT", 5000))
+    # Bind to host '0.0.0.0' to be accessible externally
+    app.run(host='0.0.0.0', port=port)
